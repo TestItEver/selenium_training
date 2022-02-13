b2b29d5b2cde57ee5a261f296d49a1ed3e311d7b
TestItEver <94520813+TestItEver@users.noreply.github.com>
2022-02-13 10:45:14 +0100

    Implemented LoginTest

--- java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LoginTest.java
@@ -6,27 +6,28 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
-public class MyFirstTest {
+public class LoginTest {
 
    private WebDriver driver;
-   private WebDriverWait wait;
 
    @Before
    public void start() {
+      //driver = new FirefoxDriver();
       driver = new ChromeDriver();
       driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-      wait = new WebDriverWait(driver, 10);
    }
 
    @Test
-   public void myFirstTest() {
-      driver.get("https://www.duckduckgo.com");
-      driver.findElement(By.name("q")).sendKeys("webdriver");
-      driver.findElement(By.id("search_button_homepage")).click();
+   public void loginTest() {
+      driver.get("http://localhost/litecart/admin/");
+      driver.findElement(By.name("username")).sendKeys("admin");
+      driver.findElement(By.name("password")).sendKeys("admin");
+      driver.findElement(By.name("login")).click();
    }
 
    @After
@@ -34,5 +35,4 @@ public class MyFirstTest {
       driver.quit();
       driver = null;
    }
-
 }
